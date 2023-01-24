2c0fca6fc610705ae0a346c7e316aff7651e6024
Katabira Derrick Teefe <derrickteefe20@gmail.com>
2023-01-25 01:37:57 +0300

    0-0, bubble-sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,25 +1,26 @@
 #include "sort.h"
+
 /**
- * bubble_sort - sort array lements from min to max value
- * @array: array
- * @size: array size
+ * bubble_sort - the worst sorting algorithm
+ * @array: array to be sorted in place
+ * @size: size of array
  */
 void bubble_sort(int *array, size_t size)
 {
+	size_t i, j;
+	int max;
 
-	size_t i, index, tmp = 0;
-
-	if (size < 2)
-		return;
-	for (i = 0; i < size; i++)
-		for (index = 0; index < size; index++)
+	for (i = size; i > 1; i--)
+	{
+		for (j = 0; j < i - 1; j++)
 		{
-			if (array[index] > array[index + 1] && array[index + 1])
+			if (array[j] > array[j + 1])
 			{
-			tmp = array[index];
-			array[index] = array[index + 1];
-			array[index + 1] = tmp;
-			print_array(array, size);
+				max = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = max;
+				print_array(array, size);
 			}
 		}
+	}
 }
